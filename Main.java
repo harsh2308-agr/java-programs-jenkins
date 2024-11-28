20ab42090e4b14aa2203fc2c09f1aecd1feb8ecc
Harsh <harshagrawal23601@gmail.com>
2024-11-28 23:59:32 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        System.err.println("This is a Java Program");
+        System.err.println("Output from java Program");
+        Date date = new Date();
+        System.out.println(date.toString());
+    }
+}
